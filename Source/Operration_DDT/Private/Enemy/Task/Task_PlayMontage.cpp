f165b8032a65a2d4dfd7acc9984c7d130d81295f
wonhow <dlgy9714@gmail.com>
2025-08-12 20:45:59 +0900

    오류 수정

--- Source/Operration_DDT/Private/Enemy/Task/Task_PlayMontage.cpp
+++ Source/Operration_DDT/Private/Enemy/Task/Task_PlayMontage.cpp
@@ -21,7 +21,7 @@ EStateTreeRunStatus UTask_PlayMontage::EnterState(FStateTreeExecutionContext& Co
 {
 	// CHelpers::GetComponent<>()
 	ACBoss* Boss=Cast<ACBoss>(Owner);
-	Boss->AttackTest(FGameplayTag::A);
+	// Boss->AttackTest(FGameplayTag::A);
 	return EStateTreeRunStatus::Running;
 }
 
